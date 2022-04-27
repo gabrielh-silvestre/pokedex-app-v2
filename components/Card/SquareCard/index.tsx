3ceab858e670cf9ae9281cf318db3813914efa3c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-27 15:20:02 -0300

    refactor(components/Card/SquareCard): add error treatment
    - try search by species after get a 404 error

--- components/Card/SquareCard/index.tsx
+++ components/Card/SquareCard/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Pokemon } from 'pokenode-ts';
 
 import NotFoundPic from '../../../public/not_found.png';
@@ -22,11 +22,31 @@ interface ISquareCardProps {
 function SquareCard({ pokemonName }: ISquareCardProps) {
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
 
-  useEffect(() => {
-    pokemonClient.getPokemonByName(pokemonName).then((pokemon) => {
-      setPokemon(pokemon);
-    });
+  const getPokemonBySpecie = useCallback(async () => {
+    const response = await pokemonClient.getPokemonSpeciesByName(pokemonName);
+    const defaultVariety = response.varieties.find(
+      ({ is_default }) => is_default
+    );
+
+    if (defaultVariety) {
+      const defaultPokemon = await pokemonClient.getPokemonByName(
+        defaultVariety.pokemon.name
+      );
+
+      setPokemon(defaultPokemon);
+    }
   }, [pokemonName]);
+
+  useEffect(() => {
+    pokemonClient
+      .getPokemonByName(pokemonName)
+      .then((pokemon) => {
+        setPokemon(pokemon);
+      })
+      .catch((err) => {
+        getPokemonBySpecie();
+      });
+  }, [pokemonName, getPokemonBySpecie]);
 
   return (
     pokemon && (
@@ -49,9 +69,13 @@ function SquareCard({ pokemonName }: ISquareCardProps) {
           <PokemonName>{pokemon.name}</PokemonName>
 
           <TypesContainer>
-            {pokemon.types.map((type) => (  // TODO
-              <span key={type.type.name}>{type.type.name}</span>
-            ))}
+            {pokemon.types.map(
+              (
+                type // TODO
+              ) => (
+                <span key={type.type.name}>{type.type.name}</span>
+              )
+            )}
             {/* <p>Tipo</p>
             <p>Tipo</p> */}
           </TypesContainer>
