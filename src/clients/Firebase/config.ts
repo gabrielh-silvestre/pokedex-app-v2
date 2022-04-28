0a8c1d052d0df1cd0edd9d040cebcc7dec677ab7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-28 20:51:41 -0300

    fix(src/clients/Firebase): stop using env variables to set Firebase

--- src/clients/Firebase/config.ts
+++ src/clients/Firebase/config.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.FB_API_KEY,
-  authDomain: process.env.FB_AUTH_DOMAIN,
-  projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.FB_MESSAGE_SENDER_ID,
-  appId: process.env.FB_APP_ID,
-  measurementId: process.env.FB_MEASUREMENT_ID,
+  apiKey: "AIzaSyD9Og1dVduRbQa4TZqCc-psvpKgt_qqVsA",
+  authDomain: "pokedex-2c6c5.firebaseapp.com",
+  projectId: "pokedex-2c6c5",
+  storageBucket: "pokedex-2c6c5.appspot.com",
+  messagingSenderId: "978809420773",
+  appId: "1:978809420773:web:87aefe6aeba4015620cd0c",
+  measurementId: "G-L8BZTNS8R1"
 };
 
 export { firebaseConfig };
