685a6a5156f8982dde204a4cf31cb022afebf1fa
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-30 16:57:58 -0300

    refactor(components/Card/SquareCard): remove priority from card images

--- components/Card/SquareCard/index.tsx
+++ components/Card/SquareCard/index.tsx
@@ -26,7 +26,6 @@ function SquareCardComponent({ id, name, types, sprites }: CardData) {
           alt={name}
           width={100}
           height={100}
-          priority
         />
       </ImageContainer>
 
