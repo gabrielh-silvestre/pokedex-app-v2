17ee47c1a4c5a096fc94ff661cb97477ef8608d7
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-04-30 17:09:09 -0300

    chore(pages/generation): disable generation page

--- pages/generation/[id].tsx
+++ pages/generation/[id].tsx
@@ -1,24 +1,21 @@
-import type { GetServerSideProps, NextPage } from 'next';
-import Head from 'next/head';
-import { NamedAPIResource } from 'pokenode-ts';
+import type { NextPage } from 'next';
+// import Head from 'next/head';
+// import { NamedAPIResource } from 'pokenode-ts';
 
-import { StaticList } from '../../components/List/StaticList';
-import { ListProvider } from '../../src/contexts/ListContext';
+// import { StaticList } from '../../components/List/StaticList';
+// import { ListProvider } from '../../src/contexts/ListContext';
 
-import { gameClient } from '../../src/clients/PokeNode';
+// import { gameClient } from '../../src/clients/PokeNode';
 
-interface IListByGenerationProps {
-  pokemonList: NamedAPIResource[];
-  generationName: string;
-}
+// interface IListByGenerationProps {
+//   pokemonList: NamedAPIResource[];
+//   generationName: string;
+// }
 
-const PokemonListByGeneration: NextPage<IListByGenerationProps> = ({
-  pokemonList,
-  generationName,
-}) => {
+const PokemonListByGeneration: NextPage = () => {
   return (
     <>
-      <Head>
+      {/* <Head>
         <title>Pokemon | {generationName}</title>
         <meta
           name="description"
@@ -29,27 +26,30 @@ const PokemonListByGeneration: NextPage<IListByGenerationProps> = ({
           property="og:description"
           content={`List of all ${generationName} pokemon`}
         />
-      </Head>
+      </Head> */}
       <>
-        <ListProvider>
+        {/* <ListProvider>
           <StaticList list={pokemonList} species />
-        </ListProvider>
+        </ListProvider> */}
+        <div className="flex justify-center items-center grow">
+          <p className="text-4xl text-gray-700">PAGE TEMPORARY DISABLED</p>
+        </div>
       </>
     </>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { id } = context.query;
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const { id } = context.query;
 
-  const response = await gameClient.getGenerationById(Number(id));
+//   const response = await gameClient.getGenerationById(Number(id));
 
-  return {
-    props: {
-      pokemonList: response.pokemon_species,
-      generationName: response.name,
-    },
-  };
-};
+//   return {
+//     props: {
+//       pokemonList: response.pokemon_species,
+//       generationName: response.name,
+//     },
+//   };
+// };
 
 export default PokemonListByGeneration;
