abd2ba08d3dceb78e3c558ca8988e67c44850135
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-07 17:35:49 -0300

    refactor(src/clients/Firebase/config): use env var

--- src/clients/Firebase/config.ts
+++ src/clients/Firebase/config.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD9Og1dVduRbQa4TZqCc-psvpKgt_qqVsA",
-  authDomain: "pokedex-2c6c5.firebaseapp.com",
-  projectId: "pokedex-2c6c5",
-  storageBucket: "pokedex-2c6c5.appspot.com",
-  messagingSenderId: "978809420773",
-  appId: "1:978809420773:web:87aefe6aeba4015620cd0c",
-  measurementId: "G-L8BZTNS8R1"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 export { firebaseConfig };
