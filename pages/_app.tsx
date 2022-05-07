a20a6c7e2bb3c98ac290cbf2195da7c80f7df2a6
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-07 16:52:15 -0300

    feat(pages/_app): implement Next Progress Bar
    - instal nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Header } from '../components/Header';
+import NextProgressBar from 'nextjs-progressbar';
 
 import { AuthProvider } from '../src/contexts/AuthContext';
+
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <AuthProvider>
+        <NextProgressBar />
         <Header />
         <Component {...pageProps} />
       </AuthProvider>
