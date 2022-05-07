d6a75d63a50a0586211f9ce46992896770ca42ca
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-07 17:11:50 -0300

    feat(components/Card/LoadingSquareCard): create generic loading card
    - render on StaticList

---
+++ components/Card/LoadingSquareCard/index.tsx
@@ -0,0 +1,16 @@
+function LoadingSquareCard() {
+  return (
+    // <!-- Create By JokerBanny -->
+    <div className="bg-white p-4 my-4 rounded-md">
+      <div className="w-64 h-44 bg-gray-200 animate-pulse"></div>
+      <div className="mt-8 h-32 w-full space-y-3">
+        <div className="w-20 h-6 bg-gray-200 rounded-full animate-pulse"></div>
+        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+        <div className="w-full h-4 bg-gray-200 rounded-full animate-pulse"></div>
+        <div className="w-1/2 h-4 bg-gray-200 rounded-full animate-pulse"></div>
+      </div>
+    </div>
+  );
+}
+
+export { LoadingSquareCard };
