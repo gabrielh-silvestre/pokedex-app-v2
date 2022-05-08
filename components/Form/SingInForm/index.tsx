6dcfc85e7c0c8ab9f50a9be24fa3982a9a93c5a3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-05-08 11:38:21 -0300

    refactor(components/Form/SingInForm): only available github signin

--- components/Form/SingInForm/index.tsx
+++ components/Form/SingInForm/index.tsx
@@ -1,26 +1,14 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
 
 import { AiFillGithub } from 'react-icons/ai';
 import { MdOutlineCatchingPokemon } from 'react-icons/md';
 
 import { useAuth } from '../../../src/contexts/AuthContext';
 
-import {
-  AuthButton,
-  Container,
-  ContentContainer,
-  Divisor,
-  DivisorText,
-  Label,
-  OptionsContainer,
-  SubmitButton,
-  TextInput,
-} from './style';
+import { AuthButton, Container, ContentContainer } from './style';
 
 function SingInForm() {
-  const { push } = useRouter();
-  const { signInWithGithub, user } = useAuth();
+  const { signInWithGithub } = useAuth();
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -30,21 +18,13 @@ function SingInForm() {
     signInWithGithub();
   };
 
-  useEffect(() => {
-    if (user) {
-      push('/');
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [user]);
-
   return (
     <>
       <Container>
         <ContentContainer>
           <MdOutlineCatchingPokemon className="w-32 h-32 mb-14 text-red-700" />
           <form onSubmit={handleSubmit}>
-            <Label>
+            {/* <Label>
               <TextInput type="text" placeholder="Email address" />
             </Label>
 
@@ -53,23 +33,23 @@ function SingInForm() {
             </Label>
 
             <OptionsContainer>
-              {/* <CheckBoxLabel htmlFor="remberCheck">
-                <CheckBox type="checkbox" id="remberCheck" defaultChecked />
+              <CheckBoxLabel htmlFor="rememberCheck">
+                <CheckBox type="checkbox" id="rememberCheck" defaultChecked />
                 Remember me
-              </CheckBoxLabel> */}
-              {/* <a
+              </CheckBoxLabel>
+              <a
                 href="#!"
                 className="text-red-600 hover:text-red-700 focus:text-red-700 active:text-red-800 duration-200 transition ease-in-out"
               >
                 Forgot password?
-              </a> */}
+              </a>
             </OptionsContainer>
 
             <SubmitButton type="submit">Sign in</SubmitButton>
 
             <Divisor>
               <DivisorText>OR</DivisorText>
-            </Divisor>
+            </Divisor> */}
 
             <AuthButton type="button" onClick={signIn}>
               <AiFillGithub className="w-6 h-6 mr-2" /> Continue with GitHub
